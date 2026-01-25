3fe2a8f3049e9a3e5597c165c81cd944b37b86ad
[Prayusha Pokhrel] <sibwawc91@gmail.com>
2026-01-25 20:52:37 +0545

    Debugging

--- script.js
+++ script.js
@@ -2,118 +2,129 @@ const input = document.getElementById("TaskInput");
 const button = document.getElementById("AddBtn");
 const list = document.getElementById("TaskList");
 
-button.addEventListener("click", function(){
-    console.log("Add button clicked!");
-});
-
-
-let tasks=[];
+let tasks = [];
 // tasks is  [ { id: 1, title: "Study", completed: false } ]
-
 
 // save to local storage
 function saveTasks() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-    // "[{"id":1,"title":"Study","completed":false}]", is string, not array
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  // "[{"id":1,"title":"Study","completed":false}]", is string, not array
 }
 
 // load from local storage
 function loadTasks() {
-    const savedTasks = localStorage.getItem("tasks"); //string
-    tasks = JSON.parse(savedTasks) || []; //parse the string to js
-    // {"id":1,"title":"Study","completed":false} will go in tasks
-    // if no savedtasks, [] ie empty array will be stored
- }
+  const savedTasks = localStorage.getItem("tasks"); //string
+  tasks = JSON.parse(savedTasks) || []; //parse the string to js
+  // {"id":1,"title":"Study","completed":false} will go in tasks
+  // if no savedtasks, [] ie empty array will be stored
+}
 
 function renderTasks() {
-    list.innerHTML = "";
+  list.innerHTML = "";
 
-    //For each task object inside the tasks array
-    tasks.forEach(task => {
-        list.innerHTML += `
+  if (tasks.length === 0) {
+    list.innerHTML = `<li class="empty>No tasks yet!!! </li>`;
+    count.textContent = "0 tasks";
+    return;
+  }
+
+  let completedCount = 0;
+
+  //For each task object inside the tasks array
+  tasks.forEach((task) => {
+    if (task.completed) completedCount++;
+
+    list.innerHTML += `
             <li data-id="${task.id}" class="${task.completed ? "completed" : ""}">
                 <span class="task-text">${task.title}</span>
                 <button class="edit">✏️</button>
                 <button class="delete">×</button>
             </li>
         `;
-    });
+  });
+
+  count.textContent = `${completedCount} / ${tasks.length} completed`;
 }
 
+input.addEventListener("input", function () {
+  // Disable button if input is empty or whitespace
+  const userInput = input.value;
+  const trimmedInput = userInput.trim();
+  const isEmpty = trimmedInput === "";
 
-button.addEventListener("click", function(){
-    const TaskText = input.value;
-
-    if (TaskText.trim() === ""){ 
-        return;
-    }
-
-    const task = {
-        id: Date.now(),
-        title: TaskText,
-        completed: false
-    };
-
-    tasks.push(task);
-    saveTasks();
-    renderTasks();
-
-    input.value=""; //to clear tasks from input field after clicking add button
+  if (isEmpty) {
+    button.disabled = true;
+  } else {
+    button.disabled = false;
+  }
 });
 
+button.addEventListener("click", function () {
+  const TaskText = input.value;
 
+  if (TaskText.trim() === "") {
+    return;
+  }
 
+  const task = {
+    id: Date.now(),
+    title: TaskText,
+    completed: false,
+  };
+
+  tasks.push(task);
+  saveTasks();
+  renderTasks();
+
+  input.value = ""; //to clear tasks from input field after clicking add button
+  button.disabled = true;
+});
 
 list.addEventListener("click", function (event) {
+  // Go up from what I clicked until you find the nearest <li>
+  const li = event.target.closest("li");
+  if (!li) return;
 
-    // Go up from what I clicked until you find the nearest <li>
-    const li = event.target.closest("li");
-    if (!li) return;
+  // <li data-id=...> to access that, we write dataset.id
+  const taskId = Number(li.dataset.id);
+  // reads <li data-id="123"> and converts it to number.
 
-    // <li data-id=...> to access that, we write dataset.id
-    const taskId = Number(li.dataset.id);
-    // reads <li data-id="123"> and converts it to number.
+  const task = tasks.find((t) => t.id === taskId);
+  // for t in tasks:
+  // if t["id"] == taskId:
+  //     return t
+  // .find() returns the actual object, Not a copy, So modifying it changes the array.
 
-    const task = tasks.find(t => t.id === taskId);
-    // for t in tasks:
-    // if t["id"] == taskId:
-    //     return t
-    // .find() returns the actual object, Not a copy, So modifying it changes the array.
+  // .filter():
+  // Loops through the array
+  // Keeps items that return true
+  // Removes items that return false
+  // Returns a new array
 
-    // .filter():
-    // Loops through the array
-    // Keeps items that return true
-    // Removes items that return false
-    // Returns a new array
+  if (event.target.classList.contains("delete")) {
+    tasks = tasks.filter((t) => t.id !== taskId);
+    // tasks = [t for t in tasks if t["id"] != taskId]
+    // Keep all tasks EXCEPT the one with this ID
+    saveTasks();
+    renderTasks();
+    return;
+  }
 
-
-    if (event.target.classList.contains("delete")) {
-        tasks = tasks.filter(t => t.id !== taskId);
-        // tasks = [t for t in tasks if t["id"] != taskId]
-        // Keep all tasks EXCEPT the one with this ID
-        saveTasks();
-        renderTasks();
-        return;
+  if (event.target.classList.contains("edit")) {
+    const newText = prompt("Edit task:", task.title);
+    if (newText && newText.trim() !== "") {
+      task.title = newText.trim();
+      saveTasks();
+      renderTasks();
     }
+    return;
+  }
 
-    if (event.target.classList.contains("edit")) {
-        const newText = prompt("Edit task:", task.title);
-        if (newText && newText.trim() !== "") {
-            task.title = newText.trim();
-            saveTasks();
-            renderTasks();
-        }
-        return;
-    }
-
-    // Toggle completed state
-    task.completed = !task.completed;
-        saveTasks();
-        renderTasks();
-
+  // Toggle completed state
+  task.completed = !task.completed;
+  saveTasks();
+  renderTasks();
 });
 
 loadTasks();
 renderTasks();
-
-
