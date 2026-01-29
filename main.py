31f33809c65f356bd7c42c86dfecf69dff9c230a
[Prayusha Pokhrel] <sibwawc91@gmail.com>
2026-01-29 12:54:29 +0545

    Updated main.py

--- main.py
+++ main.py
@@ -2,14 +2,29 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 import os
+from models import Task
 
 app = FastAPI()
 
 # Mount static folder (for CSS/JS)
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Serve HTML file on home route
 @app.get("/", response_class=HTMLResponse)
 def home():
-    with open("templates/index.html", encoding="utf-8") as f:
+    with open("templates/index.html") as f:
         return f.read()
+    
+tasks = []
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+
+@app.post("/tasks")
+def add_tasks(task: Task):
+    # task object of Task class
+    task.id = len(tasks)+1
+    tasks.append(task)
+    return task
