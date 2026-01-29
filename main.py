782d6880b4ab4e53b531fc2812051c1bcc4f1e41
[Prayusha Pokhrel] <sibwawc91@gmail.com>
2026-01-29 18:26:37 +0545

    Updated main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 import os
+from fastapi.middleware.cors import CORSMiddleware
 from models import Task
 
 app = FastAPI()
@@ -9,14 +10,22 @@ app = FastAPI()
 # Mount static folder (for CSS/JS)
 # app.mount("/static", StaticFiles(directory="static"), name="static")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"], #allow all for now
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
+
 # Serve HTML file on home route
 @app.get("/", response_class=HTMLResponse)
 def home():
     with open("templates/index.html") as f:
         return f.read()
     
-tasks = []
 
+tasks = []
 @app.get("/tasks")
 def get_tasks():
     return tasks
