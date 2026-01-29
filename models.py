abbcecd9c55d7b9bf5da113b0a891b67895e6202
[Prayusha Pokhrel] <sibwawc91@gmail.com>
2026-01-29 18:28:44 +0545

    Updated file

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
 # basemodel tells fastAPI that this is structured data
 
+
+# Pydantic models because normal Python classes cannot automatically validate and convert JSON request bodies into typed Python objects, nor generate API schemas. Pydantic handles parsing, validation, and documentation
 class Task(BaseModel):
     id: int | None = None  # id can be int or none (int|None)
     title: str
