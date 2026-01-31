3ba2adf1d95332173e352777ceee5c144504e85c
[Prayusha Pokhrel] <sibwawc91@gmail.com>
2026-01-31 19:28:12 +0545

    Adde sqlite db

---
+++ database.py
@@ -0,0 +1,98 @@
+import sqlite3
+
+DB_NAME = "tasks.db"
+
+
+def get_connection():
+    return sqlite3.connect(DB_NAME)
+# opens the db file, creates it if it doesnt exist. returns a connection object
+
+
+def create_table():
+    conn = get_connection()
+    cursor = conn.cursor()
+    # cursor is a sql command executor. it is used to run queries, fetch results
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS tasks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            completed INTEGER NOT NULL
+        )
+    """)
+
+    conn.commit()
+    # commit needed for insert/update/delete etc to actually work (kinda like confirming a task)
+    conn.close()
+
+
+def get_tasks():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT id, title, completed FROM tasks")
+    rows = cursor.fetchall()
+    # fetchall returns each row of the db, [(1,"study",0), (2,"sleep",0)]
+
+    conn.close()
+
+    tasks = []
+    # convert to dictionary
+    for row in rows:
+        tasks.append({
+            "id": row[0],
+            "title": row[1],
+            "completed": bool(row[2])
+        })
+
+    return tasks
+
+
+def add_task(title):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "INSERT INTO tasks (title, completed) VALUES (?, ?)",
+        (title, 0)
+    )
+
+    conn.commit()
+    conn.close()
+
+
+def update_task(task_id, completed):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "UPDATE tasks SET completed = ? WHERE id = ?",
+        (int(completed), task_id)
+    )
+
+    conn.commit()
+    conn.close()
+
+
+def delete_task(task_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "DELETE FROM tasks WHERE id = ?",
+        (task_id,)
+    )
+
+    conn.commit()
+    conn.close()
+
+
+
+# btw
+# | SQL Keyword | Effect           |
+# | ----------- | ---------------- |
+# | SELECT      | Read             |
+# | INSERT      | Add              |
+# | UPDATE      | Modify           |
+# | DELETE      | Remove           |
+# | CREATE      | Create structure |
